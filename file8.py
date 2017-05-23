334136544a5c2091d1da6b43c8b10ecfaa6fa560
praneelrathore <praneelrathore@gmail.com>
2017-05-23 19:39:46 +0530

    Added comments for readability

--- file8.py
+++ file8.py
@@ -7,23 +7,35 @@ import os
 #FOR PLOTTING GRAPHS AND MATRICES
 emo = ['cheerful','depressing','exciting','fun','happy','hate','joy','love','lovely','melancholy','mellow','sad','senti','sentimental','shock','terrible','very_happy']
 for e in emo:
-    path1 = "Graphs/"+e+"/Male/Combined/"
-
-    #path1 = "Graphs/"+e+"/Male/"
+    #Remeber to change male/female accordingly
+    
+    #When you will first run the file, after executing file7, uncomment [path1 = "Graphs/"+e+"/Male/"]
+    #When you will run this file for the second time after executing file9, uncomment [path1 = "Graphs/"+e+"/Male/Combined/"]
+    #path1 = "Graphs/"+e+"/Male/Combined/"
+    path1 = "Graphs/"+e+"/Male/"
     path2 = "Plots/"+e+"/Male/"
 
     for infile in os.listdir(path1):
         if not os.path.isdir(path1+infile):
             if not infile.startswith('.'):
                 #infile = "Graph_Male_02_fun.net"
-                #outfile1 = "Plot_graph_"+infile[6:13]+"_"+e+".png"
-                #outfile2 = "Plot_matrix_"+infile[6:13]+"_"+e+".png"
-                outfile1 = "Plot_graph_" + infile[16:20] + "_"+e+".png"  #FOR COMBINED PLOTS
-                outfile2 = "Plot_matrix_" + infile[16:20] + "_"+e+".png"  #FOR COMBINED PLOTS
+                #When you will first run the file, after executing file7, uncomment these two lines and comment the ones below it
+                outfile1 = "Plot_graph_"+infile[6:13]+"_"+e+".png"          #[6:13] for males, [6:15] for females
+                outfile2 = "Plot_matrix_"+infile[6:13]+"_"+e+".png"         #[6:13] for males, [6:15] for females
+                #When you will run this file for the second time after executing file9, uncomment these two lines and comment the above two.
+                #outfile1 = "Plot_graph_" + infile[16:20] + "_"+e+".png"  #FOR COMBINED PLOTS   #[16:20] for males, [16:20] for females
+                #outfile2 = "Plot_matrix_" + infile[16:20] + "_"+e+".png"  #FOR COMBINED PLOTS  #[16:20] for males, [16:20] for females
+                
+                
+                #Do not consider the commented code below
+                '''
                 #outfile3 = "Plot_graph_" + infile[6:13] + "_"+e+".png"
                 #outfile4 = "Plot_matrix_"+infile[6:13]+"_"+e+".png"
                 #outfile3 = "Plot_graph_" + infile[16:20] + "_" + e + ".png"  # FOR COMBINED PLOTS
                 #outfile4 = "Plot_matrix_" + infile[16:20] + "_" + e + ".png"  # FOR COMBINED PLOTS
+                '''
+                
+                
                 print ('infile', infile)
                 print ('outfile1', outfile1)
                 print ('outfile2', outfile2)
